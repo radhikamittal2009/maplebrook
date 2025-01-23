6b2e05bfe9f3b15049b6dc74f8661350eb6be502
radhikamittal2009 <mittalradhika2009@gmail.com>
2025-01-24 02:36:21 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Write your JS here
+// used Chatppt for the help with the JS code
+document.getElementById("hamburger").addEventListener("click", function () {
+    const navMenu = document.getElementById("nav-menu-wrapper");
+    navMenu.classList.toggle("show");
+    // Toggle the 'show' class to show/hide the menu
+});
